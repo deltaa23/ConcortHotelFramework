51482c075196322b29822fbd608d12dd8ff144d3
unknown <you@example.com>
2021-11-03 03:04:33 +0300

    v1

--- src/test/java/pages/ConcortHotelPage.java
+++ src/test/java/pages/ConcortHotelPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilitis.ConfigReader;
 import utilitis.Driver;
@@ -10,8 +12,11 @@ public class ConcortHotelPage {
        PageFactory.initElements(Driver.getDriver(),this);
     }
 
+
     public void anaSayfa(){
+
        Driver.getDriver().get(ConfigReader.getProperty("url"));
+
     }
 
 }
