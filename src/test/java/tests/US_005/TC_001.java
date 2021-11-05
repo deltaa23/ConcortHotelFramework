ca427b685076290bcaa3ded7a9c03b39b6c93b17
Huseyin Karahan <karahanhuseyin2@gmail.com>
2021-11-05 05:33:02 +0100

    uploaded

--- src/test/java/tests/US_005/TC_001.java
+++ src/test/java/tests/US_005/TC_001.java
@@ -1,6 +1,7 @@
 package tests.US_005;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
@@ -17,11 +18,11 @@ public class TC_001 {
     }
     @AfterMethod
     public void afterMethod()throws InterruptedException{
-        Thread.sleep(3000);
-        driver.quit();
+        // Thread.sleep(3000);
+        //driver.quit();
     }
     @Test
-    public void test1(){
+    public void test1() throws InterruptedException {
         driver.get(ConfigReader.getProperty("url"));
         WebElement login= driver.findElement(By.xpath("//a[.='Log in']"));
         login.click();
@@ -31,6 +32,21 @@ public class TC_001 {
         password.sendKeys("manager1!");
         WebElement button=driver.findElement(By.xpath("//input[@id='btnSubmit']"));
         button.click();
+        WebElement HotelManagement=driver.findElement(By.xpath("//span[.='Hotel Management']"));
+        HotelManagement.click();
+        WebElement HotelList=driver.findElement(By.xpath("//a[@href='/admin/HotelAdmin']"));
+        HotelList.click();
+        Thread.sleep(5000);
+        WebElement Details=driver.findElement(By.xpath("//a[@href='./HotelAdmin/Edit?Id=2206']"));
+        Details.click();
+        Thread.sleep(3000);
+        WebElement numara=driver.findElement(By.id("Code"));
+        numara.click();
+        numara.clear();
+        numara.sendKeys("123112");
+
+        WebElement button2=driver.findElement(By.xpath("//button[.='Save']"));
+        button2.click();
 
     }
 }
